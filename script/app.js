ad52781dbb770c5ba8b994979e5a448839e3c74b
RadmiloM <markovicradmilo35@gmail.com>
2026-02-14 19:23:52 +0100

    added new element to do list

--- script/app.js
+++ script/app.js
@@ -1,8 +1,15 @@
 const addBtn = document.querySelector('.add-button');
 const inputData = document.querySelector('.input-data');
+const todoListContainer = document.querySelector('.todo-list-container');
+const listContainer = document.createElement('ul');
 
+todoListContainer.insertAdjacentElement('afterend',listContainer);
 addBtn.addEventListener('click', function() {
     if(inputData.value.trim() === '') {
         alert('You must write something!')
     }
+    const listItem = document.createElement('li');
+    listItem.textContent = inputData.value;
+    listContainer.append(listItem);
+    inputData.value = '';
 })
