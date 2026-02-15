1963735a5844b955db8a050e4a44e1145401aa3f
RadmiloM <markovicradmilo35@gmail.com>
2026-02-15 17:15:23 +0100

    added hover effect on added item

--- script/app.js
+++ script/app.js
@@ -10,6 +10,16 @@ addBtn.addEventListener('click', function() {
     }
     const listItem = document.createElement('li');
     listItem.textContent = inputData.value;
+    listItem.style.cursor = 'pointer';
+    listItem.style.backgroundColor = '#f9f9f9';
+    listItem.style.padding = '10px 15px';
+    listItem.style.hover
     listContainer.append(listItem);
+    listItem.addEventListener('mouseenter', function() {
+        listItem.style.backgroundColor = '#ddd';
+    });
+    listItem.addEventListener('mouseleave', function() {
+        listItem.style.backgroundColor = '#f9f9f9'
+    })
     inputData.value = '';
 })
